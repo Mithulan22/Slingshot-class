5f44286ca6af4bbbf14619bcb92aeb9dfe479a66
Mithulan22 <76974205+Mithulan22@users.noreply.github.com>
2021-02-11 16:58:13 -0500

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,19 @@
+class Slingshot {
+constructor(bodyX, bodyY){
+var options = {
+bodyA:bodyX, 
+bodyB:bodyY,
+stiffness:0.04, 
+length:10
+
+}
+    this.ss = Constraint.create(options)
+    World.add(world,this.ss)
+}
+display(){
+var pointA = this.ss.bodyA.position
+var pointB = this.ss.bodyB.position 
+line(pointA.x ,pointA.y, pointB.x, pointB.y)
+}
+
+}
